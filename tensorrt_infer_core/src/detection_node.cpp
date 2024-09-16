3c5295a50f370d1e7802224c53f9f619117abf0d
manhha <manhha.hoang@neura-robotics>
2024-09-16 08:45:37 +0700

    update

--- tensorrt_infer_core/src/detection_node.cpp
+++ tensorrt_infer_core/src/detection_node.cpp
@@ -14,12 +14,12 @@ namespace tensorrt_infer_core
         RCLCPP_INFO(get_logger(), "Creating tensorrt detection node");
         dynamic_params_ = std::make_shared<tensorrt_infer_core::Parameters>(*this);
         dynamic_params_->setParam<float>(
-            "obj_thres", params_.detect_params.params.obj_threshold, [this](const rclcpp::Parameter &parameter)
-            { params_.detect_params.params.obj_threshold = parameter.get_value<float>(); });
+            "obj_thres", params_.detect_params.obj_threshold, [this](const rclcpp::Parameter &parameter)
+            { params_.detect_params.obj_threshold = parameter.get_value<float>(); });
 
         dynamic_params_->setParam<float>(
-            "nms_thres", params_.detect_params.params.nms_threshold, [this](const rclcpp::Parameter &parameter)
-            { params_.detect_params.params.nms_threshold = parameter.get_value<float>(); });
+            "nms_thres", params_.detect_params.nms_threshold, [this](const rclcpp::Parameter &parameter)
+            { params_.detect_params.nms_threshold = parameter.get_value<float>(); });
 
         dynamic_params_->setParam<float>(
             "seg_thres", params_.detect_params.seg_threshold, [this](const rclcpp::Parameter &parameter)
@@ -30,8 +30,8 @@ namespace tensorrt_infer_core
             { params_.detect_params.kps_threshold = parameter.get_value<float>(); });
 
         dynamic_params_->setParam<int>(
-            "num_detect", params_.detect_params.params.num_detect, [this](const rclcpp::Parameter &parameter)
-            { params_.detect_params.params.num_detect = parameter.get_value<int>(); });
+            "num_detect", params_.detect_params.num_detect, [this](const rclcpp::Parameter &parameter)
+            { params_.detect_params.num_detect = parameter.get_value<int>(); });
 
         dynamic_params_->setParam<std::vector<std::string>>(
             "detected_class", params_.detected_class, [this](const rclcpp::Parameter &parameter)
@@ -57,11 +57,29 @@ namespace tensorrt_infer_core
 
     bool DetectionNode::initModel(const std::string &model_name)
     {
-        std::filesystem::path model_path = std::filesystem::path(std::string(std::getenv("HOME"))) / "data" / "weights" / model_name;
-        std::string config_file = model_path.string() + "/config.yaml";
-        YAML::Node config = YAML::LoadFile(config_file);
-        yolo8_ = std::make_shared<tensorrt_inference::YoloV8>(model_path, config);
 
+        // config_.classNames
+        if (model_name.find("yolov8") != std::string::npos)
+        {
+            detector_ = std::make_shared<tensorrt_inference::YoloV8>(model_name);
+        }
+        else if (model_name.find("yolov9") != std::string::npos)
+        {
+            detector_ = std::make_shared<tensorrt_inference::YoloV9>(model_name);
+        }
+        else if (model_name.find("face") != std::string::npos)
+        {
+            detector_ = std::make_shared<tensorrt_inference::RetinaFace>(model_name);
+        }
+        else if (model_name.find("license_plate_detector") != std::string::npos)
+        {
+            detector_ = std::make_shared<tensorrt_inference::YoloV8>(model_name);
+        }
+        else
+        {
+            std::cout << "unkown model" << std::endl;
+            return false;
+        }
         return true;
     }
 
@@ -70,15 +88,16 @@ namespace tensorrt_infer_core
     {
         cv::Mat rgb;
         tensorrt_infer_core::rosToOpenCV(*rgb_msg, rgb);
-        if (rgb.empty() && yolo8_ == nullptr)
+        if (rgb.empty() && detector_ == nullptr)
         {
             return;
         }
         // Run inference
-        const auto objects = yolo8_->detectObjects(rgb, params_.detect_params);
+        const auto objects = detector_->detect(rgb, params_.detect_params, params_.detected_class);
         // Draw the bounding boxes on the image
-        yolo8_->drawObjectLabels(rgb, objects, params_.detect_params, params_.detected_class);
-        res_pub_->publish(*tensorrt_infer_core::openCVToRos(rgb));
+        cv::Mat result = detector_->drawObjectLabels(rgb, objects, params_.detect_params);
+        cv::cvtColor(result, result, cv::COLOR_RGB2BGR);
+        res_pub_->publish(*tensorrt_infer_core::openCVToRos(result));
     }
 
 } // namespace tensorrt_infer_core
