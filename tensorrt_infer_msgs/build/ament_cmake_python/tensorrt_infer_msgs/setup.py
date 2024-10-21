06ca3a40677121dd59fbfe24fc375f4dd1e3bd66
manhha <manhha.hoang@neura-robotics>
2024-10-21 21:35:45 +0700

    added gitignore

--- tensorrt_infer_msgs/build/ament_cmake_python/tensorrt_infer_msgs/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='tensorrt_infer_msgs',
-    version='0.0.1',
-    packages=find_packages(
-        include=('tensorrt_infer_msgs', 'tensorrt_infer_msgs.*')),
-)
--- tensorrt_infer_msgs/build/tensorrt_infer_msgs__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/neura_ai/neura/ros2_ws/src/tensorrt_inference_ros/tensorrt_infer_msgs/build/tensorrt_infer_msgs__py
-# Build directory: /home/neura_ai/neura/ros2_ws/src/tensorrt_inference_ros/tensorrt_infer_msgs/build/tensorrt_infer_msgs__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
